b191bc2b47218a80282ef26aae19a6c4bf96421d
Adriano Dantas <adrianodts@msn.com>
2024-05-21 21:46:07 -0300

    refactor: changed way to load bean

--- src/main/java/com/adrianodts/springframework/factorybeans/config/FactoryConfig.java
+++ src/main/java/com/adrianodts/springframework/factorybeans/config/FactoryConfig.java
@@ -1,6 +1,5 @@
 package com.adrianodts.springframework.factorybeans.config;
 
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -23,14 +22,12 @@ public class FactoryConfig {
         
     }
     
-    @Bean
-    @Qualifier //("portugueseHelloService")
+    @Bean(name = "portugueseHelloService")
     public HelloService portugueseHelloService() throws Exception {
         return FactoryBean.create(getLanguage());
     }
 
-    @Bean
-    @Qualifier //("spanishHelloService")
+    @Bean(name = "spanishHelloService")
     public HelloService spanishHelloService() throws Exception {
         return FactoryBean.create(getLanguage());
     }
