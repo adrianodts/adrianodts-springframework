9d8efe4228a04a7a528af93b2566dd37c0aed178
Adriano Dantas <adrianodts@msn.com>
2024-05-23 21:45:09 -0300

    test: implemented test with different profiles

---
+++ xml-configurationa/src/test/java/com/adrianodts/springframework/xmlconfiguration/xmlconfigurationa/EnglishServiceTest.java
@@ -0,0 +1,26 @@
+package com.adrianodts.springframework.xmlconfiguration.xmlconfigurationa;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import com.adrianodts.springframework.xmlconfiguration.FactoryService;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(locations = {"classpath:/spring/spring-config.xml",
+        "classpath*:/spring/spring-config-english.xml"})
+public class EnglishServiceTest {
+
+    @Autowired
+    FactoryService factoryService;
+
+    @Test
+    public void testHelloWorld(){
+        String greeting = factoryService.getMessage();
+        assertEquals("Hello! I am glad to see you", greeting);
+    }
+}
